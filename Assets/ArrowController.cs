db027bccfb12a61ecc82578f98d4c5d0b2862405
kurehajime <xiidec@gmail.com>
2017-09-18 19:25:58 +0900

    Revert "Revert "Initial commit""
    This reverts commit 1581acb27e2a6cbdd96dbe12d309de069032c797.

---
+++ Assets/ArrowController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour {
+	GameObject player;
+	PlayerController playerController;
+	// Use this for initialization
+	void Start () {
+		this.player = GameObject.Find ("player");
+		this.playerController = this.player.GetComponent<PlayerController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (0, -0.1f, 0);
+
+		if (transform.position.y < -4.2f) {
+			Destroy(gameObject);
+		}
+		if (hitTest()) {
+			Destroy(gameObject);
+			GameObject director=GameObject.Find("GameDirector");
+			director.GetComponent<GameDirector> ().DecreaseHp();
+		}
+	}
+	bool hitTest(){
+		Vector2 p1 = transform.position;
+		Vector2 p2 = this.player.transform.position;
+		Vector2 dir = p1 - p2;
+		float d = dir.magnitude;
+		float r1 = 0.5f;//矢の半径
+		float r2 = 1.0f*this.playerController.size;//プレイヤーの半径
+		return (d<r1+r2);
+	}
+}
---
+++ Assets/GameDirector.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameDirector : MonoBehaviour {
+	GameObject hpGage;
+	GameObject point;
+	float hit=1;
+	float score=0;
+
+	// Use this for initialization
+	void Start () {
+		this.hpGage = GameObject.Find ("hpGage");
+		this.point = GameObject.Find ("point");
+		Reset ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void Reset(){
+		this.hit = 1;
+		this.score = 0;
+		this.hpGage.GetComponent<Image> ().fillAmount =1f;
+		this.point.GetComponent<Text> ().text = this.score.ToString ();
+	}
+	public void DecreaseHp(){
+		this.hpGage.GetComponent<Image> ().fillAmount -= 0.1f;	
+		this.hit -= 0.1f;
+		if (this.hit < 0) {
+			this.Reset ();
+		}
+		GameObject player= GameObject.Find ("player") ;
+		player.GetComponent<PlayerController> ().oneDown();
+
+	}
+	public void AddPoint(){
+		this.score+= 1f;
+		this.point.GetComponent<Text> ().text = this.score.ToString ();
+		GameObject player= GameObject.Find ("player") ;
+		player.GetComponent<PlayerController> ().oneUP();
+	}
+}
